d9f357bd68c0937fb920cf6b4316d122a1613f98
Yuri Krupnik <krupnik.yuri@gmail.com>
2023-04-29 15:39:54 +0300

    changed rust operator

--- apps/rust/master_operator/src/main.rs
+++ apps/rust/master_operator/src/main.rs
@@ -1,92 +1,48 @@
-// copied from: https://github.com/cloud-native-skunkworks/rust-operator
-//! Generated types support documentation
-#![deny(missing_docs)]
-use anyhow::Ok;
-use futures::{pin_mut, TryStreamExt};
-
-use tracing::*;
-use apiexts::CustomResourceDefinition;
-use k8s_openapi::{apiextensions_apiserver::pkg::apis::apiextensions::v1 as apiexts, api::core::v1::Node};
-use k8s_openapi::api::core::v1::Pod;
-
+use k8s_openapi::api::batch::v1::Job;
 use kube::{
-  api::{Api, Patch, PatchParams,ListParams},
+  api::{Api, DeleteParams, PostParams},
   runtime::wait::{await_condition, conditions},
-  runtime::{watcher, WatchStreamExt},
-  Client, CustomResourceExt,
+  Client,
 };
-
-
-mod crd;
-use crd::{Topology, TopologySpec};
-
-const CRD_NAME : &str = "topologies.yurikrupnik.dev";
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
   tracing_subscriber::fmt::init();
   let client = Client::try_default().await?;
+  let jobs: Api<Job> = Api::default_namespaced(client);
 
-  let ssapply = PatchParams::apply("topology_apply").force();
-  // 0. Ensure the CRD is installed, could do this once
-  let crds: Api<CustomResourceDefinition> = Api::all(client.clone());
-  info!("Creating crd: {}", serde_yaml::to_string(&Topology::crd())?);
-  crds.patch(CRD_NAME, &ssapply, &Patch::Apply(Topology::crd()))
-    .await?;
+  info!("Creating job");
+  let name = "empty-job";
+  let data = serde_json::from_value(serde_json::json!({
+        "apiVersion": "batch/v1",
+        "kind": "Job",
+        "metadata": {
+            "name": name,
+        },
+        "spec": {
+            "template": {
+                "metadata": {
+                    "name": "empty-job-pod"
+                },
+                "spec": {
+                    "containers": [{
+                        "name": "empty",
+                        "image": "alpine:latest"
+                    }],
+                    "restartPolicy": "Never",
+                }
+            }
+        }
+    }))?;
+  jobs.create(&PostParams::default(), &data).await?;
 
-  info!("Waiting for the api-server to accept the CRD");
-  let establish = await_condition(crds, CRD_NAME, conditions::is_crd_established());
-  let _ = tokio::time::timeout(std::time::Duration::from_secs(10), establish).await?;
-
-  // Let's get the current node topology
-  // let nodes: Api<Node> = Api::all(client.clone());
-  // New client copy to inject our resource
-  let topologys: Api<Topology> = Api::default_namespaced(client.clone());
-  // let topologys: Api<Topology> = Api::
-
-  // let spec = create_spec(nodes).await;
-  //
-  // // println!("omg pods!!! {pods:?}");
-  // println!("omg topologys!!! {topologys:?}");
-  //
-  // let tt = topologys.patch("default", &ssapply,
-  //                          &Patch::Apply(&Topology::new("default", spec))).await?;
-  //
-  // info!("Applied 1 {}: {:?}", tt.name_any(), tt.spec);
+  info!("Waiting for job to complete");
+  let cond = await_condition(jobs.clone(), name, conditions::is_job_completed());
+  let _ = tokio::time::timeout(std::time::Duration::from_secs(20), cond).await?;
 
 
-  // watch the topology resources
-  let obs = watcher(topologys, ListParams::default()).applied_objects();
-  pin_mut!(obs);
-  while let Some(o) = obs.try_next().await? {
-    let _node = o;
-    {
-      let nodes: Api<Node> = Api::all(client.clone());
-      let spec = create_spec(nodes.clone()).await;
-      let topologys: Api<Topology> = Api::default_namespaced(client.clone());
-
-      let pods: Api<Pod> = Api::default_namespaced(client.clone());
-      println!("omg pods!!! {pods:?}");
-      println!("omg topologys!!! {topologys:?}");
-      let _tt = topologys.patch("default",
-                                &ssapply,
-                                &Patch::Apply(&Topology::new("default", spec))).await?;
-    }
-  }
-
+  info!("Cleaning up job record");
+  jobs.delete(name, &DeleteParams::background()).await?;
   Ok(())
-}
-async fn create_spec(nodes: Api<Node>) -> TopologySpec {
-
-  let node_list = nodes.list(&ListParams::default()).await.unwrap();
-  let mut node_names = Vec::new();
-  for node in node_list {
-    node_names.push(node.metadata.name.unwrap());
-  }
-  TopologySpec {
-    name: "default".to_string(),
-    info: "my info".to_string(),
-    nodes: node_names,
-    image: Some("nginx".to_string())
-  }
 }
